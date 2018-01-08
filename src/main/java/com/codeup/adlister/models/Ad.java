f91e1ec87346f2e304aa0fc866cef1122fe3b681
Cory Imel <cory.imel@gmail.com>
2018-01-08 14:07:35 -0600

    working on categories

--- src/main/java/com/codeup/adlister/models/Ad.java
+++ src/main/java/com/codeup/adlister/models/Ad.java
@@ -4,22 +4,25 @@ package com.codeup.adlister.models;
 public class Ad {
     private long id;
     private long userId;
+    private long catId;
     private String title;
     private String description;
     private String date;
     private String location;
 
-    public Ad(long id, long userId, String title, String description, String location, String date) {
+    public Ad(long id, long userId,long catID, String title, String description, String location, String date) {
         this.id = id;
         this.userId = userId;
         this.title = title;
         this.description = description;
         this.date = date;
         this.location = location;
+        this.catId = catID;
     }
 
-    public Ad(long userId, String title, String description, String date, String location) {
+    public Ad(long userId, long catID, String title, String description, String date, String location) {
         this.userId = userId;
+        this.catId = catID;
         this.title = title;
         this.description = description;
         this.date = date;
@@ -72,5 +75,13 @@ public class Ad {
 
     public void setLocation(String location) {
         this.location = location;
+    }
+
+    public long getCatId() {
+        return catId;
+    }
+
+    public void setCatId(long catId) {
+        this.catId = catId;
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -10,6 +10,13 @@
     <jsp:include page="/WEB-INF/partials/navbar.jsp" />
     <div class="container">
         <h1>Welcome to the Adlister!</h1>
+        <form action="/search" method="POST">
+            <div class="form-group">
+                <label for="input">Username</label>
+                <input id="input" name="input" class="form-control" type="text">
+            </div>
+            <input type="submit" class="btn btn-primary btn-block" value="Search">
+        </form>
     </div>
 </body>
 </html>
