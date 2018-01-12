efc722318acc8ec9891105897d4400deda9efc3f
nicholekirlin <nichole.kirlin@gmail.com>
2018-01-12 14:36:37 -0600

    update edit profile

--- src/main/java/com/codeup/adlister/dao/Users.java
+++ src/main/java/com/codeup/adlister/dao/Users.java
@@ -7,6 +7,8 @@ import java.util.List;
 public interface Users {
     User findByUsername(String username);
 
+    User findById(long id);
+
     Long insert(User user);
 
     void updateUser(User user);
